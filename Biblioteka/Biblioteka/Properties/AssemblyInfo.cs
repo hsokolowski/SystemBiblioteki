1dde318a845115c96155169b4b5355c4d6c25db8
Hubert Sokołowski <hubert.sokool@outlook.com>
2018-10-14 21:41:27 +0200

    push#1

---
+++ Biblioteka/Biblioteka/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informacje ogólne o zestawie zależą od poniższego 
+// zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// związane z zestawem.
+[assembly: AssemblyTitle("Biblioteka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Biblioteka")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie dla atrybutu ComVisible wartości false powoduje, że typy w tym zestawie stają się niewidoczne 
+// dla składników COM. Jeśli musisz uzyskiwać dostęp do typu w tym zestawie 
+// z modelu COM, ustaw dla atrybutu ComVisible tego typu wartość true.
+[assembly: ComVisible(false)]
+
+// Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+[assembly: Guid("e3b5336b-b7c7-4065-b40e-c3404ba11f92")]
+
+// Informacje o wersji zestawu obejmują następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów poprawki i kompilacji, 
+// stosując znak „*”, jak pokazano poniżej:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
