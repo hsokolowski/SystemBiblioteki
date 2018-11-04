8e4753f55feff02992aec00732f4ccc218f67bad
Archer141 <ezio.141.96@gmail.com>
2018-11-04 16:56:10 +0100

    Navbar improvement

--- Biblioteka/Biblioteka/ModelView/BookVM.cs
+++ Biblioteka/Biblioteka/ModelView/BookVM.cs
@@ -9,6 +9,19 @@ namespace Biblioteka.ModelView
 {
     public class BookVM
     {
+        public DB baza()
+        {
+            DB dB = new DB();
+            
+
+            //string query = "SELECT * FROM AUTHOR A "
+            //    + "JOIN BOOK B ON A.ID_AUTHOR=B.ID_AUTHOR"
+            //    + "JOIN CATEGORIES C ON B.ID_CATEGORY=C.ID_CATEGORY";
+
+            //IEnumerable<EnrollmentDateGroup> data = dB.Database.SqlQuery<EnrollmentDateGroup>(query);
+
+            return dB;
+        }
         public List<Book> Get_list()
         {
             DB mDb = new DB();
